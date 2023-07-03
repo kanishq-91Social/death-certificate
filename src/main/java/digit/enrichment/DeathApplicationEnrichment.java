111ec5cad4bf0d9ef1121f3bd630e31993867280
Kanishq Bhatnagar <kanishq@onequince.com>
2023-07-03 17:58:44 +0530

    idgen changes

--- src/main/java/digit/enrichment/DeathApplicationEnrichment.java
+++ src/main/java/digit/enrichment/DeathApplicationEnrichment.java
@@ -49,7 +49,7 @@ public class DeathApplicationEnrichment {
 //        }
 
         //Retrieve list of IDs from IDGen service
-        List<String> deathRegistrationIdList = idgenUtil.getIdList(deathRegistrationRequest.getRequestInfo(), deathRegistrationRequest.getDeathRegistrationApplications().get(0).getTenantId(), "btr.registrationid", "", deathRegistrationRequest.getDeathRegistrationApplications().size());
+        List<String> deathRegistrationIdList = idgenUtil.getIdList(deathRegistrationRequest.getRequestInfo(), deathRegistrationRequest.getDeathRegistrationApplications().get(0).getTenantId(), "dtr.registrationid", "", deathRegistrationRequest.getDeathRegistrationApplications().size());
         Integer index = 0;
         for(DeathRegistrationApplication application : deathRegistrationRequest.getDeathRegistrationApplications()) {
             // Enrich audit details
